839ad7ebb895d2aa958de56dfbbcc7e14915a8cc
Hatsumi-Mihari <testingblock78@gmail.com>
2026-08-23 12:46:36 +0000

    init backend branch

--- public/api/endpoints.ts
+++
@@ -1,9 +0,0 @@
-export const endpoints = {
-    CONFIG: "/api/v1/config.json",
-    CONTENT: {
-        HEADER: "/api/v1/header.json",
-        PAGES: "/api/v1/Content.json",
-        INDEX_TABLE: "/api/v1/index_table.json",
-        INDEX_SEARCH: "/api/v1/index_search.json"
-    }
-}
