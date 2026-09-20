7b411ccf96ea92cae6cf858f4ebfd3ce03061338
Hatsumi-Mihari <testingblock78@gmail.com>
2026-09-20 17:47:16 +0200

    deploy dev version

--- vite.config.ts
+++ vite.config.ts
@@ -10,7 +10,7 @@ export default defineConfig(({ mode }: ConfigEnv): UserConfig => {
 
   return {
     plugins: [react(), svgr(), 
-        !isDev && {
+        isDev && {
         name: 'strip-debug-code',
         transform(code, id) {
           if (/\.(mjs|js|ts|jsx|tsx)$/.test(id)) {
