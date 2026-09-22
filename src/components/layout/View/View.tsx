7cd8cb82482af6678f5b207eb78a2f0522d099c5
Hatsumi-Mihari <testingblock78@gmail.com>
2026-09-22 15:32:16 +0200

    null

---
+++ src/components/layout/View/View.tsx
@@ -0,0 +1,15 @@
+import './View.scss'
+
+
+export type Views = 'debug' | 'carts' | 'map' | 'loader'; 
+
+
+function ViewLayout() {
+    return ( 
+    <div className='View'>
+        1234
+    </div> 
+    );
+}
+
+export default ViewLayout;
